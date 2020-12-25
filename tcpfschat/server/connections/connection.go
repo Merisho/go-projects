df0f8f8f590ab24123db3f7f5f7f299b0c78fae6
Merisho <merishot@gmail.com>
2020-12-25 11:31:41 +0200

    Embed net.Conn interface into clientConn

--- tcpfschat/server/connections/connection.go
+++ tcpfschat/server/connections/connection.go
@@ -2,7 +2,6 @@ package connections
 
 import (
     "net"
-    "time"
 )
 
 type Conn interface {
@@ -12,48 +11,16 @@ type Conn interface {
 
 func newConnection(conn net.Conn, id []byte) Conn {
     return &clientConn{
-        c: conn,
+        Conn: conn,
         id: id,
     }
 }
 
 type clientConn struct {
-    c net.Conn
+    net.Conn
     id []byte
 }
 
 func (c *clientConn) ID() []byte {
     return c.id
-}
-
-func (c *clientConn) Read(b []byte) (n int, err error) {
-    return c.c.Read(b)
-}
-
-func (c *clientConn) Write(b []byte) (n int, err error) {
-    return c.c.Write(b)
-}
-
-func (c *clientConn) Close() error {
-    return c.c.Close()
-}
-
-func (c *clientConn) LocalAddr() net.Addr {
-    return c.c.LocalAddr()
-}
-
-func (c *clientConn) RemoteAddr() net.Addr {
-    return c.c.RemoteAddr()
-}
-
-func (c *clientConn) SetDeadline(t time.Time) error {
-    return c.c.SetDeadline(t)
-}
-
-func (c *clientConn) SetReadDeadline(t time.Time) error {
-    return c.c.SetReadDeadline(t)
-}
-
-func (c *clientConn) SetWriteDeadline(t time.Time) error {
-    return c.c.SetWriteDeadline(t)
 }
